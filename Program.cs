0dcd768864ad77bb33eb0476fa05a1fea55504bc
Арсений <arseny.potapov2012@yandex.ru>
2021-12-17 23:06:30 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace String_1
+{
+    public class Logic
+    {
+        public static int Change(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1] && char.IsLetter(str[i])) count++;
+            }
+            return count;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string str = Console.ReadLine();
+                
+                Console.WriteLine(Logic.Change(str));
+
+
+
+            }
+        }
+    }
+}
+
---
+++ String_1Tests/LogicTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String_1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace String_1.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void ChangeTest()
+        {
+            string str = "процессор ппочта ммашина ккарусель";
+
+            var count = Logic.Change(str);
+            Assert.AreEqual(4,count);
+        }
+        [TestMethod()]
+        public void ChangeTest_1()
+        {
+
+            string str = "деррево шинна колонна";
+
+            var count = Logic.Change(str);
+            Assert.AreEqual(3, count);
+
+        }
+        [TestMethod()]
+        public void ChangeTest_2()
+        {
+
+            string str = "пппп дом";
+
+            var count = Logic.Change(str);
+            Assert.AreEqual(3, count);
+
+        }
+
+    } 
+   
+
+}
